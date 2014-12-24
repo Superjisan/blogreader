03d496434f6e299709e5bc0b9f1d3c8e472f2a5b
Jisan Zaman <jzaman@wesleyan.edu>
2014-12-24 15:20:49 -0500

    fixed the jsonObject file due to loop

--- app/src/main/java/com/superjisan/blogreader/MainListActivity.java
+++ app/src/main/java/com/superjisan/blogreader/MainListActivity.java
@@ -2,32 +2,61 @@ package com.superjisan.blogreader;
 
 import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class MainListActivity extends ListActivity {
 
 
-    protected String[] superHeroes = {
-            "Superman",
-            "Batman",
-            "Green Lantern",
-            "Wonder Woman",
-            "Flash",
-            "Martian Manhunter",
-            "Hawkgirl"
-    };
+    protected String[] mBlogPostTitles;
+    public static final int NUMBER_OF_POSTS = 10;
+    public static String TAG = MainListActivity.class.getSimpleName();
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, superHeroes);
-        setListAdapter(adapter);
+        if(isNetworkAvailable()) {
+            GetBlogPostsTask getBlogPostsTask = new GetBlogPostsTask();
+            getBlogPostsTask.execute();
+        }else{
+            Toast.makeText(this, "Network is unavailable", Toast.LENGTH_LONG);
+        }
+
+//        Toast.makeText(this, message, Toast.LENGTH_LONG);
+    }
+
+    private boolean isNetworkAvailable(){
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+        boolean isAvailable = false;
+        if(networkInfo != null && networkInfo.isConnected()){
+            isAvailable = true;
+        }
+
+        return isAvailable;
     }
 
 
@@ -48,5 +77,58 @@ public class MainListActivity extends ListActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private class GetBlogPostsTask extends AsyncTask<Object, Void, String>{
+
+        @Override
+        protected String doInBackground(Object... objects) {
+            int responseCode = -1;
+            try {
+                URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=" + NUMBER_OF_POSTS);
+                HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
+                connection.connect();
+
+                responseCode = connection.getResponseCode();
+                if(responseCode == HttpURLConnection.HTTP_OK){
+                    InputStream inputStream = connection.getInputStream();
+                    Reader reader = new InputStreamReader(inputStream);
+                    int nextCharacter; // read() returns an int, we cast it to char later
+                    String responseData = "";
+                    while(true){ // Infinite loop, can only be stopped by a "break" statement
+                        nextCharacter = reader.read(); // read() without parameters returns one character
+                        if(nextCharacter == -1) // A return value of -1 means that we reached the end
+                            break;
+                        responseData += (char) nextCharacter; // The += operator appends the character to the end of the string
+                    }
+
+                    JSONObject jsonResponse = new JSONObject(responseData);
+                    String status = jsonResponse.getString("status");
+                    Log.v(TAG, status);
+
+                    JSONArray jsonPosts = jsonResponse.getJSONArray("posts");
+                    for(int i = 0; i < jsonPosts.length(); i++){
+                        JSONObject jsonPost = jsonPosts.getJSONObject(i);
+                        String title = jsonPost.getString("title");
+                        Log.i(TAG, title);
+                    }
+
+
+                }else{
+                    Log.e(TAG, "Unsuccessful HTTP Response Code: " + responseCode);
+                }
+                Log.i(TAG, "Code: " +  responseCode);
+            }catch(MalformedURLException e){
+                Log.e(TAG, "Malformed URL exception:", e);
+            }
+            catch(IOException e){
+                Log.e(TAG, "IO Exception Caught: ", e);
+            }
+            catch(Exception e){
+                Log.e(TAG, "Exception caught :", e);
+            };
+
+            return "Code" + responseCode;
+        }
     }
 }
